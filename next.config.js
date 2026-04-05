c3606c1e63294adbe3f4177ca277622c0bfa9f84
Musa <mr.musausman@gmail.com>
2026-04-05 13:37:48 +0300

    fix: set turbopack root in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
+};
 module.exports = nextConfig;
